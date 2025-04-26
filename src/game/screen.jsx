45e85512154b06b103568442c7a47b81a1a42f7a
JoseAlfredo0201 <A01645391@tec.mx>
2025-04-26 16:37:04 -0600

    Update screen.jsx
    added pokemon batltle scene

--- src/game/screen.jsx
+++ src/game/screen.jsx
@@ -1,28 +1,51 @@
-const Screen = ({ pokemones }) => {
-  console.log(pokemones);
+const Screen = ({ pokemones, hoverPokemon, selectedPokemones }) => {
+  console.log(selectedPokemones);
 
   return (
     <div className="container-screen">
       <div className="screen-text">
-
         <div className="screen">
-         {pokemones?.map((pokemon) => (
-          <div>
-          <img src={pokemon.sprites.front_default} alt="pokemones"/>
-          <p className="pokemon-name">{pokemon.name}</p>
-          </div>
-         ))}
+          {selectedPokemones.length === 2 ? (
+            <div>
+              <div style={{display:"flex", justifyContent:"flex-end" }}>
+                <div>
+                  <img src={selectedPokemones[0][0].sprites?.front_default} alt="imagen1"/>
+                </div>
+              </div>
+              <div style={{ width:"315px", height:"50%" }}>
+              <div>
+                  <img src={selectedPokemones[1][0].sprites?.back_default} alt="imagen1"/>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+              {pokemones?.map((pokemon) => (
+                <div
+                  style={{
+                    backgroundColor: `${
+                      hoverPokemon === pokemon.id ? 'yellow' : ''
+                    }`,
+                  }}
+                >
+                  <img src={pokemon.sprites.front_default} alt="pokemones" />
+                  <p style={{ fontFamily: 'Pokemon Classic', fontSize: '8px' }}>
+                    {pokemon.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
         <div className="container-text">
           <p className="text">
             Nintendo <span>GAME BOY</span>
             <span style={{ fontSize: '8px' }}> TM</span>
           </p>
-          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Screen;
-// import React from "react";
+export default Screen; 
