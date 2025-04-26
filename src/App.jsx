aa0d9ef513d27f18c1de4903e28a23103d2caf9d
JoseAlfredo0201 <A01645391@tec.mx>
2025-04-26 16:40:17 -0600

    Update App.jsx
    pokemon imp

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import StartSelect from "./game/buttons/StartSelect.jsx";
 import Screen from "./game/screen.jsx";
 function App() {
   const [pokemones, setPokemones] = useState([]);
-
+  const [hoverPokemon, setHoverPokemon] = useState(0);
+  const [selectedPokemon, setSelectedPokemon] = useState([]);
   const Base_URL = "https://pokeapi.co/api/v2/";
 
 const getPokemones = async() => {
@@ -23,8 +24,39 @@ const getDetails = async (results) => {
     return data;
 }
 
-const handlePress = (direction) => {
-  console.log(direction);
+const handlePress = (dir) => {
+  console.log(dir);
+  if (dir === 'right') {
+    setHoverPokemon(hoverPokemon + 1);
+  }
+  if (dir === 'left') {
+    setHoverPokemon(hoverPokemon - 1);
+  }
+  if (dir === 'up') {
+    setHoverPokemon(hoverPokemon - 3);
+  }
+  if (dir === 'down') {
+    setHoverPokemon(hoverPokemon + 3);
+  }
+};
+
+const handleSelectPokemon = () => {
+  console.log('select pokemon', hoverPokemon);
+  const pokemonSelected = pokemones.filter(
+    (pokemon) => pokemon.id === hoverPokemon
+  );
+
+  console.log({pokemonSelected});
+
+  computerSelection();
+  const selection = [pokemonSelected, computerSelection()];
+  setSelectedPokemon(selection);
+};
+
+const computerSelection = () => {
+  const randomID = Math.floor(Math.random() * pokemones.length);
+  const selectElement = pokemones.filter(pokemon => pokemon.id === randomID);
+  return selectElement;
 }
 
   useEffect(() => {
@@ -42,11 +74,11 @@ const handlePress = (direction) => {
           style={{ width: "420px", height: "600px", border: "2px solid black", borderRadius: "5px 5px 35px 10px"}}
         >
           {/* container screen */}
-          <Screen pokemones = {pokemones} />
+          <Screen pokemones={pokemones} hoverPokemon={hoverPokemon} selectedPokemones={selectedPokemon} />
           {/* container buttons */} 
           <div style ={{display: "flex", justifyContent: "space-around"}}>
             <Pad handlePress = {handlePress}/>
-            <StartSelect />
+            <StartSelect handleSelectPokemon = {handleSelectPokemon}/>
             {/* A B buttons */}
             <Actions />
           </div>
